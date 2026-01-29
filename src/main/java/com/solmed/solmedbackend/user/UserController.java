ae423c1b4f733ba24effeb5191191337e2da0f64
Developer <dev@solmed.com>
2026-01-29 12:19:21 +0530

    commit

--- src/main/java/com/solmed/solmedbackend/user/UserController.java
+++ src/main/java/com/solmed/solmedbackend/user/UserController.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
@@ -13,6 +16,12 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @PostMapping("/addUser")
+    public User postUser(@RequestBody User user) {
+         return userService.addUser(user);
+        
+    }
+    
     @GetMapping("/getUserById/{id}")
     public User getUserById(@PathVariable Long id){
         return userService.getUserId(id);
--- src/main/java/com/solmed/solmedbackend/user/UserService.java
+++ src/main/java/com/solmed/solmedbackend/user/UserService.java
@@ -14,4 +14,8 @@ public class UserService {
         return userrepo.findById(id).orElse(null);
     }
 
+    public User addUser(User user) {
+       return userrepo.save(user);
+    }
+
 }
